5eeb3d489e1e53cddaa0f6d420ec84a05260af98
kanitsharma <kanitsharma@protonmail.com>
2023-08-03 16:31:29 +0530

    Added semaphore

---
+++ semaphore.ts
@@ -0,0 +1,67 @@
+export {}
+
+interface Worker {
+    Resolver: (_: any) => void,
+    Task: <T>() => Promise<T>
+}
+
+class Semaphore{
+    Pool: Array<Worker>
+    MaxConcurrency: number
+    Executing: number
+
+    constructor(concurrency: number) {
+        this.Executing = 0;
+        this.MaxConcurrency = concurrency;
+        this.Pool = new Array();
+    }
+
+    addTask(task: <T>() => Promise<T>) {
+        return new Promise(resolve => {
+            this.Pool.push({
+                Task: task,
+                Resolver: resolve
+            })
+
+            this.execute();
+        })
+    }
+
+    private execute() {
+        if (this.Pool.length === 0) return;
+        if (this.Executing >= this.MaxConcurrency) return;
+
+        const { Task, Resolver } = this.Pool.shift() as Worker;
+
+        this.Executing++;
+        Task().then(Resolver).finally(() => {
+            this.Executing--;
+            this.execute();
+        });
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+async function main() {
+    const tasks = new Array(100);
+    const pool = new Semaphore(25);
+
+    for (const _ of tasks) {
+        pool.addTask(() => Process()).then(i =>   console.log("Processed Item " + i));
+    }
+}
+
+
+
+function Process(): Promise<any> {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(getRandomInt(200));
+        }, 1000);
+    });
+}
+
+await main();
--- simple-worker-pool.ts
+++ simple-worker-pool.ts
@@ -17,18 +17,22 @@ class WorkerPool<T> {
 
 async function main() {
     const tasks = new Array(100);
-    const pool = new WorkerPool<unknown>(10);
+    const pool = new WorkerPool<unknown>(25);
 
     for (const _ of tasks) {
         await pool.runTask(index => Process(index));
     }
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}  
+
 function Process(workerID: Number) {
     return new Promise(r => setTimeout(() => {
         console.log(`Processsing item ${workerID}`);
         r(true);
-    }, 1000));
+    }, getRandomInt(1000)));
 }
 
 await main();
